6a518b39add646c190bd0189c129c394214cdbb6
Connie Park <conniedc1206@gmail.com>
2022-05-23 10:03:07 -0700

    commit tree 82e496cb51ed6b218e7df69841cd2f85889ca0ae

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+//remove #main
+const main = document.querySelector("#main");
+main.remove();
+
+//the 'newHeader' variable points to a node 'h1#victory'
+//has \"YOUR-NAME is the champion\" inside
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion'
